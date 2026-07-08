dac33600bc23f0082553556559e34e1407d9cee3
Mannat Gupta <mannatgupta146@gmail.com>
2026-07-08 17:21:19 +0530

    feat: implement automatic token refresh interceptor for 401 unauthorized errors in axiosInstance

--- src/config/axiosInstance.jsx
+++ src/config/axiosInstance.jsx
@@ -4,3 +4,23 @@ export const axiosInstance = axios.create({
     baseURL: "/api",
     withCredentials: true
 })
+
+axiosInstance.interceptors.response.use(
+    (response)=> response,
+    async (error)=> {
+        const originalReq = error.config
+
+        if(error.response.status === 401 && !originalReq._retry) {
+            originalReq._retry = true
+
+            try {
+                await axiosInstance.get("/auth/get-accessToken")
+                return axiosInstance(originalReq)
+                
+            } catch (error) {
+                window.location.href = "/auth/login"
+                return Promise.reject(error)
+            }
+        }
+    }
+)
